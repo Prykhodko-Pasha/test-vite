55fcdc09fd9f3ada282d8b9fc47f2d2b6623c925
Prykhodko-Pasha <prykhodko.p@gmail.com>
2024-02-08 17:38:34 +0200

    seventh try

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
+      "/": {
         target: "https://singaporemath.online:3001/api/v1",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\//, ""),
       },
     },
   },
